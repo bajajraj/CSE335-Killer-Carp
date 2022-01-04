80d7cd21c48f19c08c648cdccd68b96d2a0118ab
bajajraj <91573783+bajajraj@users.noreply.github.com>
2022-01-04 00:49:58 -0500

    Add files via upload

---
+++ AquariumApp.h
@@ -0,0 +1,24 @@
+/**
+ * @file AquariumApp.h
+ * @author Rajaditya Shrikishan Bajaj
+ *
+ * Class responsible for making of the aquarium.
+ */
+
+#ifndef AQUARIUM_AQUARIUMAPP_H
+#define AQUARIUM_AQUARIUMAPP_H
+
+#include <wx/wx.h>
+
+/**
+ * Class responsible for making of the aquarium.
+ */
+class AquariumApp : public wxApp{
+private:
+
+public:
+
+    virtual bool OnInit();
+};
+
+#endif //AQUARIUM_AQUARIUMAPP_H
---
+++ main.cpp
@@ -0,0 +1,9 @@
+/**
+ * @file main.cpp
+ * @author Rajaditya Shrikishan Bajaj
+ */
+
+#include "pch.h"
+#include "AquariumApp.h"
+
+wxIMPLEMENT_APP(AquariumApp);
